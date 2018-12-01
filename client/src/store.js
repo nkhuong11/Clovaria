39ed044e2729d6c589d3f8715608638361773b14
nkhuong11 <khuong.nguyen0322@gmail.com>
2018-12-01 17:05:42 +0700

    run both client and server

--- client/src/store.js
+++ client/src/store.js
@@ -8,6 +8,6 @@ const store = createStore(
     rootReducer, 
     inititalState, 
     compose(applyMiddleware(thunk), 
-            window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()));
+            window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__() || compose ));
 
 export default store;
